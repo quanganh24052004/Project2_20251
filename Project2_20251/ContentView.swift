bef957b6df007e0f95ae4b6923a655b84bfe998d
Anh Nguyen Quang <anh.nguyenquang24052004@gmail.com>
2025-09-22 12:37:00 +0700

    Initial Commit

---
+++ Project2_20251/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Project2_20251
+//
+//  Created by Nguyễn Quang Anh on 22/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Project2_20251/Project2_20251App.swift
@@ -0,0 +1,17 @@
+//
+//  Project2_20251App.swift
+//  Project2_20251
+//
+//  Created by Nguyễn Quang Anh on 22/9/25.
+//
+
+import SwiftUI
+
+@main
+struct Project2_20251App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
